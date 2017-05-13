ed246867c6760e1c0eafd18291130922cc88c786
Nathan Gaberel <nathang@theodo.co.uk>
2017-05-13 17:58:06 +0100

    Add CI.

--- web/functions/index.js
+++ web/functions/index.js
@@ -1,8 +1,8 @@
-var functions = require('firebase-functions');
+var functions = require('firebase-functions')
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+// Start writing Firebase Functions
+// https://firebase.google.com/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!')
+})
