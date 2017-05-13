4459b34c2ff36af1ce18754b796f74f7f1289f7b
Nathan Gaberel <nathang@theodo.co.uk>
2017-05-13 17:49:24 +0100

    Add react app.

---
+++ web/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true
+  }
+}
