75490e63b597020371df0ac7436df3d9dc1142e5
Nathan Gaberel <nathang@theodo.co.uk>
2017-06-19 17:25:24 +0100

    Add logout.

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,11 +1,27 @@
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+import { logout } from '../redux/reducers/user.actions'
 
 class Header extends PureComponent {
   render () {
     return <header>
-      <h1>🐈</h1>
+      <h1 onClick={this.props.logout}>🐈</h1>
     </header>
   }
 }
 
-export default Header
+Header.propTypes = {
+  logout: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = {
+  logout
+}
+
+const HeaderContainer = connect(mapStateToProps, mapDispatchToProps)(Header)
+
+export default HeaderContainer
